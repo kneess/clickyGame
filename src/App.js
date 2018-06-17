5de560e5529a99560ac569dfa701f5095318d5ad
knees <you@example.com>
2018-06-16 19:30:20 -0700

    game recognizes when picture is clicked twice and ends game: keeps top score: resets idArray to start new game

--- src/App.js
+++ src/App.js
@@ -10,16 +10,49 @@ class App extends Component  {
     trees,
     score: 0,
     topScore: 0,
-    isClicked: false
+    idArray: []
   };
 
-   // handleIncrement increases this.state.count by 1
-   handleIncrement = event => {
-    event.preventDefault();
-    // We always use the setState method to update a component's state
-    this.setState({ score: this.state.score + 1 });
-  };
+  //  // handleIncrement increases this.state.count by 1
+  //  handleIncrement = event => {
+  //   //  console.log(this.state.isClicked)
+  //   event.preventDefault();
+  //   // We always use the setState method to update a component's state
+  //   this.setState({ score: this.state.score + 1 });
+  // };
+  endGame = () => {
+    this.setState({score: 0, idArray: []});
+    this.highScore();
+    }
+  highScore = () => {
+    if(this.state.score > this.state.topScore) {
+      this.setState({ topScore: this.state.score})
+    } else 
+    this.setState({ topScore: this.state.topScore})
+  }
 
+  changeClickedStatus= id => {
+    console.log(id)
+    
+   
+    const newArray = this.state.idArray;
+    console.log("newArray", newArray);
+   
+    if(this.state.idArray.includes(id)) {
+       this.endGame();
+    } else {
+      newArray.push(id);
+      this.setState({ score: this.state.score + 1 })
+    }
+      // newState = {
+      // score: this.state.score + 1,
+      // topScore: this.state.topScore + 1,
+      //   }  
+   
+      // this.setState(newState);
+      // console.log("newState", newState);
+      // this.randomize();
+      }
   
 
 
@@ -31,8 +64,8 @@ render() {
       <Cards
       id={tree.id}
       image={tree.image}
-      handleIncrement={this.handleIncrement}
-      isClicked={this.state.isClicked}
+      // handleIncrement={this.handleIncrement}
+      changeClickedStatus={this.changeClickedStatus}
       />
     ))}
     <Footer />
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,7 +4,7 @@ import "./Cards.css"
 const Cards = props => (
     <div className="card">
     <div className="img-container">
-        <img className="img" alt={props.name} src={props.image} onClick={props.handleIncrement} />
+        <img className="img" alt={props.name} src={props.image} onClick={() =>props.changeClickedStatus(props.id)} id={props.id}/>
         </div>
         </div>
 );
